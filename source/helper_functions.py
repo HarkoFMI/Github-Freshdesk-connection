16cc37a09a5b63da199201997e46f136f5cac145
Harut Partamian <harutpartamian@gmail.com>
2023-05-08 12:15:34 +0300

    Add creating/updating freshdesk contact functionality

--- source/helper_functions.py
+++ source/helper_functions.py
@@ -1,13 +1,11 @@
-from pprint import pprint
-
 import requests
-
+from pprint import pprint
 from token_configuration import GITHUB_TOKEN, FRESHDESK_TOKEN
 
 
 def github_user_info(username: str):
     url = f"https://api.github.com/users/{username}"
-    response = requests.get(url, auth=(f"{username}", f"{GITHUB_TOKEN}"))
+    response = requests.get(url, auth=(username, GITHUB_TOKEN))
 
     if response.status_code != 200:
         raise Exception(f"Error while getting information from Github API")
@@ -15,7 +13,7 @@ def github_user_info(username: str):
     return response.json()
 
 
-pprint(github_user_info("HarkoFMI"))
+# pprint(github_user_info("HarkoFMI"))
 
 
 def create_payload(user_info):
@@ -28,6 +26,8 @@ def create_payload(user_info):
 
     if user_info["email"]:
         payload["email"] = user_info["email"]
+    else:
+        raise Exception("Cannot create contact without email!")
     if user_info["location"]:
         payload["address"] = user_info["location"]
     if user_info["bio"]:
@@ -36,7 +36,31 @@ def create_payload(user_info):
     return payload
 
 
-pprint(create_payload(github_user_info("HarkoFMI")))
+# pprint(create_payload(github_user_info("HarkoFMI")))
 
 
+def make_freshdesk_contact(github_username: str, domain: str):
+    user_info = github_user_info(github_username)
+    payload = create_payload(user_info)
 
+    url = f"https://{domain}.freshdesk.com/api/v2/contacts"
+    response = requests.get(url, auth=(FRESHDESK_TOKEN, "X"))
+    for contact in response.json():
+        if contact["name"] == user_info["name"]:
+            request = requests.put(f'https://{domain}.freshdesk.com/api/v2/contacts/{contact["id"]}', json=payload,
+                                   auth=(FRESHDESK_TOKEN, "X"))
+
+            if 199 < request.status_code < 300:
+                raise Exception(
+                    "Error with updating the freshdesk contact with query status code: {request.status_code}")
+
+            return "Updated contact successfully"
+
+    request = requests.post(f'https://{domain}.freshdesk.com/api/v2/contacts', json=payload,
+                            auth=(FRESHDESK_TOKEN, "X"))
+    if not 199 < request.status_code < 300:
+        raise Exception(f"Error with creating the freshdesk contact with query status code: {request.status_code}")
+
+    return "Created contact successfully"
+
+# print(make_freshdesk_contact("HarkoFMI", "harutpartamian"))
