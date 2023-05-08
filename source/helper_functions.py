4e032c6b5aa9332cf3e486504a97a4cccd012737
Harut Partamian <harutpartamian@gmail.com>
2023-05-08 11:05:13 +0300

    Add contact payload creation functionality

--- source/helper_functions.py
+++ source/helper_functions.py
@@ -1,11 +1,13 @@
 from pprint import pprint
-from token_configuration import GITHUB_TOKEN
-from requests import get
+
+import requests
+
+from token_configuration import GITHUB_TOKEN, FRESHDESK_TOKEN
 
 
 def github_user_info(username: str):
     url = f"https://api.github.com/users/{username}"
-    response = get(url, auth=(f"{username}", f"{GITHUB_TOKEN}"))
+    response = requests.get(url, auth=(f"{username}", f"{GITHUB_TOKEN}"))
 
     if response.status_code != 200:
         raise Exception(f"Error while getting information from Github API")
@@ -14,3 +16,27 @@ def github_user_info(username: str):
 
 
 pprint(github_user_info("HarkoFMI"))
+
+
+def create_payload(user_info):
+    payload = {}
+
+    if user_info["name"]:
+        payload["name"] = user_info["name"]
+    else:
+        raise Exception("Cannot create contact without name!")
+
+    if user_info["email"]:
+        payload["email"] = user_info["email"]
+    if user_info["location"]:
+        payload["address"] = user_info["location"]
+    if user_info["bio"]:
+        payload["description"] = user_info["bio"]
+
+    return payload
+
+
+pprint(create_payload(github_user_info("HarkoFMI")))
+
+
+
